fb54c14388990b7e4bc8433b306dad1c02d93a8e
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2025-01-22 18:30:54 -0500

    ADD route name to 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NotFound() {
+  const pathname = usePathname();
+
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center overflow-hidden scrollbar-thin">
       <h2>Not Found</h2>
-      <p>This resource is either legacy, myth or unbegun.</p>
+      <p>{pathname} is either legacy, myth, or unbegun.</p>
       <Link href="/" className="text-gray-500">
         Return Home
       </Link>
