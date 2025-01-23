8a8bd50f8aacec7b0113927c0e12734d5c73dd17
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2025-01-23 09:27:36 -0500

    ADD cloud title url

--- src/app/components/right-cloud.tsx
+++ src/app/components/right-cloud.tsx
@@ -11,9 +11,16 @@ interface RightCloudProps {
 }
 
 export default function RightCloud({ title, items }: RightCloudProps) {
+  const titleLink = `https://${title}.intxr.net`;
+
   return (
     <div className="relative border border-gray-300 p-4 rounded-lg w-1/2">
-      <h2 className="absolute -top-3 right-2 bg-white px-2 text-sm">{title}</h2>
+      <Link
+        href={titleLink}
+        className="absolute -top-3 left-2 bg-white px-2 text-sm hover:underline"
+      >
+        {title}
+      </Link>
       <ul className="space-y-2 text-center">
         {items.map((item, index) => (
           <li key={index}>
