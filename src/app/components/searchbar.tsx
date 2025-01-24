7c90b94d536b2102ea68cb3922c0957a4cfe26b9
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2025-01-24 14:32:48 -0500

    ADD ease of life changes

--- src/app/components/searchbar.tsx
+++ src/app/components/searchbar.tsx
@@ -23,16 +23,34 @@ export default function Searchbar({ onSearch }: SearchbarProps) {
     debouncedSearch(query);
   };
 
+  const handleClear = () => {
+    setSearchQuery("");
+    onSearch("");
+  };
+
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.select();
+  };
+
   return (
     <div className="relative w-1/2 mx-auto mb-8">
       <span className="absolute -top-3 left-2 bg-white px-2 text-sm">
         search
       </span>
+      {searchQuery && (
+        <span
+          className="absolute -bottom-2 right-2 bg-white px-2 text-sm hover:underline cursor-pointer"
+          onClick={handleClear}
+        >
+          clear
+        </span>
+      )}
       <input
         type="text"
         placeholder=""
         value={searchQuery}
         onChange={handleSearchChange}
+        onFocus={handleFocus}
         className="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
       />
     </div>
