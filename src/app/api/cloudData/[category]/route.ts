03727e0dfe74162d4dd6d411ccf0cd13206470fc
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2025-02-10 08:17:32 -0500

    ADD metadata api

---
+++ src/app/api/cloudData/[category]/route.ts
@@ -0,0 +1,16 @@
+import { cloudData } from "@/app/data";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  context: { params: { category: string } }
+) {
+  const category = context.params.category;
+
+  if (!cloudData[category]) {
+    return NextResponse.json({ error: "Category not found" }, { status: 404 });
+  }
+
+  // Return only the items array, excluding the 'side' property
+  return NextResponse.json({ items: cloudData[category].items });
+}
